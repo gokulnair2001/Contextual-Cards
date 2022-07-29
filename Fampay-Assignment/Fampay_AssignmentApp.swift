25baae1f17959178383a912908f40770f2479e41
Gokul Nair <gokulnair.2001@gmail.com>
2022-07-29 18:16:41 +0530

    Initial Commit

---
+++ Fampay-Assignment/Fampay_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fampay_AssignmentApp.swift
+//  Fampay-Assignment
+//
+//  Created by Gokul Nair on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Fampay_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
