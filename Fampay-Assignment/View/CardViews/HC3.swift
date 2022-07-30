baedf3efe72cff1a758f3339b2869d9e724824dd
Gokul Nair <gokulnair.2001@gmail.com>
2022-07-30 12:05:47 +0530

    HC3 card view animation setup

--- Fampay-Assignment/View/CardViews/HC3.swift
+++ Fampay-Assignment/View/CardViews/HC3.swift
@@ -18,8 +18,6 @@ struct HC3: View {
                 HStack {
                     ForEach(Card.cards) { card in
                         hc3CardUI(card: card)
-                        // .frame(width: UIScreen.main.bounds.width)
-                        //.cornerRadius(10)
                     }
                 }
             }
@@ -38,46 +36,13 @@ struct HC3: View {
 extension HC3 {
     @ViewBuilder
     func hc3CardUI(card: Card) -> some View {
-        ZStack(alignment: .bottom) {
-            AsyncImage(url: URL(string: card.bgImage?.imageURL ?? "")) { image in
-                image.resizable()
-                    .aspectRatio(card.bgImage?.aspectRatio ?? 1, contentMode: .fit)
-            } placeholder: {
-                Color(hexStringToUIColor(hex: card.bgColor ?? "#000000"))
-            }//.aspectRatio(card.bgImage?.aspectRatio ?? 1, contentMode: .fill)
+        ZStack {
             
-            VStack(alignment: .leading, spacing: 28) {
-                
-                Text(card.title ?? "Error")
-                    .font(.roboto(weight: .medium, size: 30))
-                    .padding(.leading, 20)
-                    .padding(.trailing, 27)
-                    .lineLimit(2)
-                    .multilineTextAlignment(.leading)
-                    .hLeading()
-                
-                Text(card.cardDescription ?? "Error")
-                    .font(.roboto(weight: .regular, size: 12))
-                    .padding(.leading, 20)
-                    .padding(.trailing, 40)
-                    .lineLimit(2)
-                    .multilineTextAlignment(.leading)
-                    .hLeading()
-                
-                HStack{
-                    Button{
-                        print("Action")
-                    }label: {
-                        Text("Action")
-                            .font(.roboto(weight: .medium))
-                            .foregroundColor(.white)
-                            .frame(width: 128, height: 42, alignment: .center)
-                            .background(.black)
-                            .cornerRadius(6)
-                    }
-                }.padding(.leading, 20)
-            }
-            .frame(width: UIScreen.main.bounds.width-60, height: (400-125))
+            Color.white
+            
+            hc3BottomCardUI(card: card)
+            
+            hc3CardTopUI(card: card)
             
         }.frame(height: 400, alignment: .center)
             .shadow(color: .gray.opacity(0.5), radius: 3, x: 0, y: 0)
@@ -85,8 +50,101 @@ extension HC3 {
             .onTapGesture {
                 openURL(URL(string: card.url)!)
             }
+            .onLongPressGesture {
+                print("long pressss")
+            }
+    }
+    
+    
+    // MARK: - Top Visible Card UI
+    @ViewBuilder
+    func hc3CardTopUI(card: Card) -> some View {
+        AsyncImage(url: URL(string: card.bgImage?.imageURL ?? "")) { image in
+            image.resizable()
+                .aspectRatio(card.bgImage?.aspectRatio ?? 1, contentMode: .fit)
+        } placeholder: {
+            Color(hexStringToUIColor(hex: card.bgColor ?? "#000000"))
+        }
+        
+        VStack(alignment: .leading, spacing: 28) {
+            
+            Text(card.title ?? "Error")
+                .font(.roboto(weight: .medium, size: 30))
+                .padding(.leading, 20)
+                .padding(.trailing, 27)
+                .lineLimit(2)
+                .multilineTextAlignment(.leading)
+                .hLeading()
+            
+            Text(card.cardDescription ?? "Error")
+                .font(.roboto(weight: .regular, size: 12))
+                .padding(.leading, 20)
+                .padding(.trailing, 40)
+                .lineLimit(2)
+                .multilineTextAlignment(.leading)
+                .hLeading()
+            
+            HStack{
+                Button{
+                    print("Action")
+                }label: {
+                    Text("Action")
+                        .font(.roboto(weight: .medium))
+                        .foregroundColor(.white)
+                        .frame(width: 128, height: 42, alignment: .center)
+                        .background(.black)
+                        .cornerRadius(6)
+                }
+            }.padding(.leading, 20)
+        }
+        .frame(width: UIScreen.main.bounds.width-60, height: (400-125))
+        .padding(.top, 125)
+    }
+    
+    
+    // MARK: - Card Bottom UI
+    @ViewBuilder
+    func hc3BottomCardUI(card: Card) -> some View {
+        VStack(spacing: 37) {
+            
+            hc3Button(image: "Subtract", title: "remind later") {
+                print("subtract card")
+            }
+            
+            hc3Button(image: "Dismiss", title: "dismiss now") {
+                print("dismiss button")
+            }
+            
+        }
+        .padding(.leading, 21)
+        .hLeading()
+    }
+    
+    
+    // MARK: - Custom Button Builder
+    @ViewBuilder
+    func hc3Button(image:String, title: String ,action: @escaping (()->Void)) -> some View {
+        Button {
+            
+            action()
+            
+        }label: {
+            VStack {
+                Image(image)
+                    .resizable()
+                    .frame(width: 20, height: 20, alignment: .center)
+                
+                Text(title)
+                    .font(.roboto(weight: .regular, size: 10))
+                    .foregroundColor(.black)
+            }
+            .frame(width: 72, height: 59)
+            .background(Color(hexStringToUIColor(hex: "#F7F6F3")))
+            .cornerRadius(12)
+        }
     }
 }
+
 
 //struct HC3_Previews: PreviewProvider {
 //    static var previews: some View {
